b63246d056ccdd60f152ca20b544f930daa0581a
meir <lightir21@gmail.com>
2023-03-02 05:11:01 +0200

    sending client to serve in server

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
 
@@ -32,13 +35,17 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.get("/api/v1", (req, res) => {
-  res.json({ msg: "well met!" });
-});
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/drivers", driverRouter);
 app.use("/api/v1/job", jobRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
